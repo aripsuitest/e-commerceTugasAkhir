30959ca3f9af62931a7ae11c33ec28e6300449f6
kopsus <emonsetio01@gmail.com>
2025-06-03 12:42:59 +0700

    fix : handle logout

--- src/app/api/auth/logout/route.js
+++ src/app/api/auth/logout/route.js
@@ -1,22 +1,21 @@
-import { NextResponse } from 'next/server';
+import { NextResponse } from "next/server";
 
 export async function GET(req) {
-  const url = new URL('/auth/login', req.url);
-  const response = NextResponse.redirect(url);
+  const response = NextResponse.redirect("/auth/login");
 
-  response.cookies.set('token', '', {
+  response.cookies.set("token", "", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    path: '/',
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    path: "/",
     expires: new Date(0),
   });
 
-  response.cookies.set('user', '', {
+  response.cookies.set("user", "", {
     httpOnly: true,
-    secure: process.env.NODE_ENV === 'production',
-    sameSite: 'strict',
-    path: '/',
+    secure: process.env.NODE_ENV === "production",
+    sameSite: "strict",
+    path: "/",
     expires: new Date(0),
   });
 
